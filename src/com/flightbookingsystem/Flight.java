9ae0da0bed1d8ec9abb124e7aa8c1735a39d5454
PranavbalajiGit <pranavbalaji65690@gmail.com>
2026-03-09 21:28:50 +0530

    Flight module Implemented

--- src/com/flightbookingsystem/Flight.java
+++ src/com/flightbookingsystem/Flight.java
@@ -1,4 +1,32 @@
 package com.flightbookingsystem;
 
 public class Flight {
+    private String flightNo;
+    private String source;
+    private String destination;
+    private double price;
+
+    public Flight(String flightNo , String source , String destination , double price) {
+        this.flightNo = flightNo;
+        this.source = source;
+        this.destination = destination;
+        this.price = price;
+    }
+
+    public String getId() {
+        return flightNo;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
+
