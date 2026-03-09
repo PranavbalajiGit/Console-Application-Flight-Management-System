7f998854921021e073dbfec778cd62e40219f4fc
PranavbalajiGit <pranavbalaji65690@gmail.com>
2026-03-09 22:25:49 +0530

    Admin Module Implemented

--- src/com/flightbookingsystem/User.java
+++ src/com/flightbookingsystem/User.java
@@ -3,10 +3,10 @@ package com.flightbookingsystem;
 //This is a Abstract class and admin class will implement the method showMenu.
 abstract public class User {
     //This is protected because it must be used in Admin class
-    protected String user;
+    protected String name;
 
-    public User (String user) {
-        this.user = user;
+    public User (String name) {
+        this.name = name;
     }
 
     public abstract void showMenu();
