7ba6cf51b9bbedc6390b783c6609d1bde6f0c0a2
PranavbalajiGit <pranavbalaji65690@gmail.com>
2026-03-14 16:21:07 +0530

    Implemented Switch case for User Choices from UserMenu

--- src/com/flightbookingsystem/FlightBookingApp.java
+++ src/com/flightbookingsystem/FlightBookingApp.java
@@ -1,4 +1,45 @@
 package com.flightbookingsystem;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class FlightBookingApp {
+    Scanner sc = new Scanner(System.in);
+    Admin admin = new Admin("Pranav");
+
+    List<Passenger> passenger = new ArrayList<>();
+    List<Flight> flight = new ArrayList<>();
+    List<Booking> bookings = new ArrayList<>();
+
+    while(true) {
+        admin.showMenu();
+        System.out.println("Enter your Choice : ");
+        int choice = sc.nextInt();
+        sc.nextLine();
+
+        switch (choice) {
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            case 5:
+                break;
+
+            case 6:
+                break;
+
+            default:
+                System.out.println("Invalid Choice . . .");
+        }
+    }
 }
